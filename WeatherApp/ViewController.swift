7733aecf6a20e99ad7ab7a717ddd37b5c7b70184
Henrik Frank <frank.havsvinge@gmail.com>
2020-02-04 13:19:50 +0100

    Init commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Henrik Frank on 2020-02-03.
+//  Copyright © 2020 Henrik Frank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
