04cd8d95abd025cfc174ba92f8cf3c675f7d9300
Henrik Frank <frank.havsvinge@gmail.com>
2020-02-12 13:48:08 +0100

    Made current tab bar item unclickable when on that tab

---
+++ WeatherApp/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  WeatherApp
+//
+//  Created by Henrik Frank on 2020-02-12.
+//  Copyright © 2020 Henrik Frank. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    var currentTab: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.delegate = self
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        print(currentTab)
+        
+        if self.tabBar.selectedItem?.tag == currentTab {
+            
+            return false
+        }
+        
+        currentTab = self.tabBar.selectedItem?.tag ?? -1
+        
+        return true
+    }
+
+}
